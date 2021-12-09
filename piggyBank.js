6dc96aa63874a157fef8ba53a0f914c42da1712b
Jess Izumi <izumijessica@gmail.com>
2021-12-09 15:05:32 -0500

    added localStorage code

--- piggyBank.js
+++ piggyBank.js
@@ -17,6 +17,7 @@ var yyyy = today.getFullYear()
 today = mm + '/' + dd + '/' + yyyy
 const todaysDate = document.getElementById('todaysDate')
 const endDate = document.getElementById('endDate')
+
 /*Save Button Event Listener*/
 saveButton.addEventListener('click', function () {
   /*define function to determine end date*/
@@ -126,16 +127,19 @@ saveButton.addEventListener('click', function () {
     `<br>` +
     choreFiveResults
 
-  displayy.innerHTML = Math.min(
+  let totalWeeks = Math.min(
     choreOneTotal,
     choreTwoTotal,
     choreThreeTotal,
     choreFourTotal,
     choreFiveTotal
   )
+
+  displayy.innerHTML = totalWeeks
   displayyList.innerHTML = finalDisplayString
   todaysDate.innerHTML = today
-  endDate.innerHTML = minDate.toLocaleDateString('en-US')
+  let endDateValue = minDate.toLocaleDateString('en-US')
+  endDate.innerHTML = endDateValue
 
   // LocalStorage - Save
   // Save goal input
@@ -146,7 +150,15 @@ saveButton.addEventListener('click', function () {
   localStorage.setItem('choreThree', JSON.stringify(choreThreeInt))
   localStorage.setItem('choreFour', JSON.stringify(choreFourInt))
   localStorage.setItem('choreFive', JSON.stringify(choreFiveInt))
+  // Save total weeks
+  localStorage.setItem('totalWks', JSON.stringify(totalWeeks))
+  // Save End Date
+  localStorage.setItem('endDateVal', JSON.stringify(endDateValue))
+  // Save finalDisplayString
+  localStorage.setItem('breakdown', JSON.stringify(finalDisplayString))
 })
+
+// Verify Input
 function inputVerify(
   choreOne,
   choreTwo,
@@ -178,12 +190,101 @@ const choreTwoAmount = JSON.parse(localStorage.getItem('choreTwo'))
 const choreThreeAmount = JSON.parse(localStorage.getItem('choreThree'))
 const choreFourAmount = JSON.parse(localStorage.getItem('choreFour'))
 const choreFiveAmount = JSON.parse(localStorage.getItem('choreFive'))
-console.log(today)
-console.log(goalAmount)
-console.log(choreOneAmount)
-console.log(choreTwoAmount)
-console.log(choreThreeAmount)
-console.log(choreFourAmount)
-console.log(choreFiveAmount)
+const totalWeeks = JSON.parse(localStorage.getItem('totalWks'))
+const endDateVal = JSON.parse(localStorage.getItem('endDateVal'))
+const breakdown = JSON.parse(localStorage.getItem('breakdown'))
 
-//Add stored data to date in dropdown
+//Display stored data on dropdown click
+const pastPlan = document.getElementById('past-planOutput')
+const savedPlan = document.getElementById('saved-plan')
+
+function displayPastPlan() {
+  var newDiv = document.createElement('div')
+  let h1_startDate = document.createElement('h1')
+  h1_startDate.textContent = 'Start Date:' + today
+  pastPlan.appendChild(newDiv)
+  newDiv.appendChild(h1_startDate)
+
+  let goalPast =
+    '$' + goalAmount + ' - ' + '<i class="fas fa-trophy"></i> Goal' + '\n'
+  let choreOnePast =
+    '$' +
+    choreOneAmount +
+    '/wk - ' +
+    '<i class="fas fa-broom"></i> Vacumming' +
+    '\n'
+  let choreTwoPast =
+    '$' +
+    choreTwoAmount +
+    '/wk - ' +
+    '<i class="fas fa-utensils"></i> Dishes' +
+    '\n'
+  let choreThreePast =
+    '$' +
+    choreThreeAmount +
+    '/wk - ' +
+    '<i class="fas fa-tshirt"></i> Folding Clothes' +
+    '\n'
+  let choreFourPast =
+    '$' +
+    choreFourAmount +
+    '/wk - ' +
+    '<i class="fas fa-broom"></i> Mopping' +
+    '\n'
+  let choreFivePast =
+    '$' +
+    choreFiveAmount +
+    '/wk - ' +
+    '<i class="fas fa-car"></i> Washing Car' +
+    '\n'
+
+  let pastDisplayString =
+    choreOnePast +
+    `<br>` +
+    choreTwoPast +
+    `<br>` +
+    choreThreePast +
+    `<br>` +
+    choreFourPast +
+    `<br>` +
+    choreFivePast
+
+  let h1_goal = document.createElement('h1')
+  h1_goal.style.color = 'green'
+  h1_goal.innerHTML = goalPast
+  newDiv.appendChild(h1_goal)
+
+  let h1_income = document.createElement('h1')
+  h1_income.innerHTML = 'Income:' + '<br>' + pastDisplayString
+  pastPlan.appendChild(newDiv)
+  newDiv.appendChild(h1_income)
+
+  let h1_totalWks = document.createElement('h1')
+  h1_totalWks.textContent = 'Total Weeks: ' + totalWeeks + ' weeks'
+  pastPlan.appendChild(newDiv)
+  newDiv.appendChild(h1_totalWks)
+
+  let h1_breakdown = document.createElement('h1')
+  h1_breakdown.textContent = 'Breakdown: '
+  console.log(breakdown)
+  pastPlan.appendChild(newDiv)
+  newDiv.appendChild(h1_breakdown)
+
+  let h1_endDate = document.createElement('h1')
+  h1_endDate.textContent = 'End Date: ' + endDateVal
+  pastPlan.appendChild(newDiv)
+  newDiv.appendChild(h1_endDate)
+}
+
+//Display past plan of date
+savedPlan.addEventListener('change', function () {
+  //var selectedText = savedPlan.options[savedPlan.selectedIndex].innerHTML
+  var selectedValue = savedPlan.value
+  console.log(selectedValue)
+  var i = 1
+
+  if (i === 1 && selectedValue === 'savingsTwo') {
+    displayPastPlan()
+    i++
+  }
+})
